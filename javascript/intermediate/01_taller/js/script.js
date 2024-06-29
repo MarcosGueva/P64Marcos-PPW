da1191b42ccf1df6b38531c9ac3b72492d180ed9
MarcosGueva <mguevaras2@est.ups.edu.ec>
2024-06-28 21:38:51 -0500

    intermedio

---
+++ javascript/intermediate/01_taller/js/script.js
@@ -0,0 +1,17 @@
+var texto="Programacion y Plataformas Web"
+
+function imprimir_01(){
+    let texto2="Avengers"
+    var texto3="Liga de la Justicia"
+    console.log(texto)
+    console.log(texto2)
+    console.log(texto3)
+}
+function imprimir_02(){
+    console.log(texto)
+    console.log(texto2)
+    console.log(texto3)
+}
+
+imprimir_01()
+imprimir_02()
